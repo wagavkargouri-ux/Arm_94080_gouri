8bb93a9e4db791549a1b02991957c05033366f79
Gouri Wagavkar <wagavkargouri@gmail.com>
2025-12-31 08:54:13 +0530

    assi_3

---
+++ assi_3/3_1/Core/Src/my_switch.c
@@ -0,0 +1,41 @@
+/*
+ * my_switch.c
+ *
+ *  Created on: Dec 17, 2025
+ *      Author: kiran_z6dopa8
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include"stm32f407xx.h"
+#include"my_switch.h"
+
+
+void switch_init(void)
+{
+	//0. enable clock for GPIOA in AHB1
+	RCC->AHB1ENR |= BV(0);
+	//1. select mode as Input
+	GPIOA->MODER &= ~(BV(0) | BV(1) );
+
+
+	//2. select type as push pull
+	//GPIOA->OTYPER &= ~(BV(0) );
+	//3. select speed as low
+	GPIOA->OSPEEDR &= ~(BV(0) | BV(1));
+
+	//4. select pull up/down as no
+	GPIOA->PUPDR &= ~(BV(0) | BV(1));
+
+}
+int switch_status(void)
+{
+	if ((GPIOA->IDR) & BV(0) )
+	{
+		return 1 ; // switch is press
+	}
+	else
+	{
+		return 0 ; // switch is nor press
+	}
+}
